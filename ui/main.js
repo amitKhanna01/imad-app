c973cbac238fe84b28f0795a364488223b2ee672
Amit Khanna <30940074+amitKhanna01@users.noreply.github.com>
2017-08-18 15:20:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="Amit Khanna";
 
-var img =document.getElementById('amit');
-img.onclick=function()
+var img = document.getElementById('amit');
+img.onclick = function()
 {
-    img.style.marginleft='100px';
+  img.styel.marginleft='100px';  
 };
